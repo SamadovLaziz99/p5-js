cd87ff1689a06fcefd13f03a5bf34e8649c791b1
SamadovLaziz99 <lazizbeksamadov99@gmail.com>
2023-02-24 11:18:47 +0500

    shar tegdi

--- sketch.js
+++ sketch.js
@@ -10,12 +10,19 @@ class Shar {
     }
 }
 
+let sharStop = false;
+
 const shar_1 = new Shar(50, 50, 80, 80, 1, 1, 1);
 const shar_2 = new Shar(150, 50, 80, 80, 2, -1, 1);
 
 const box = {
     width: 600,
     height: 400,
+}
+
+function sharStopFalse(event) {
+    this.sharStop = event;
+    return this.sharStop;
 }
 
 function setup() {
@@ -31,11 +38,22 @@ function draw() {
     let yCoords = shar_2.y - shar_1.y;
     let radius = shar_1.w / 2 + shar_2.w / 2;
 
-
     if (Math.pow(radius, 2) >= (Math.pow(xCoords, 2) + Math.pow(yCoords, 2))) {
-        console.log("ichida");
+        console.log("tegdi");
+        sharStop = false;
+        console.log('sharStop1: ', sharStop);
         shar_1.speed = 0;
         shar_2.speed = 0;
+        // console.log(Math.pow(radius, 2), Math.pow(xCoords, 2) + Math.pow(yCoords, 2));
+        shar_1.vx = shar_1.vx * (-1);
+        shar_1.vy = shar_1.vy * (-1);
+        shar_2.vx = shar_2.vx * (-1);
+        shar_2.vy = shar_2.vy * (-1);
+        this.sharStopFalse(true);
+        if(this.sharStopFalse) {
+            shar_1.speed = 1;
+            shar_2.speed = 2;
+        }
     }
 
     if ((shar_1.x === (box.width - shar_1.h / 2) && shar_1.vx === 1) || (shar_1.x === shar_1.h / 2 && shar_1.vx === -1)) {
@@ -56,7 +74,4 @@ function draw() {
     shar_1.y = shar_1.y + shar_1.vy * shar_1.speed;
     shar_2.x = shar_2.x + shar_2.vx * shar_2.speed;
     shar_2.y = shar_2.y + shar_2.vy * shar_2.speed;
-    //     if (Math.pow((el.w / 2 + el.w / 2), 2) === Math.pow((el.y - el.y), 2) + Math.pow((el.x - el.x), 2)) {
-    //         console.log("chich");
-    //     }
 }
